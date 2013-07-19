66ec2b4f606143a254013a53286ef985677b4fa4
laurakalbag <github@laurakalbag.com>
2013-07-19 13:48:57 +0100

    Wrap header around navigation too

--- perch/templates/layouts/global/header.php
+++ perch/templates/layouts/global/header.php
@@ -1,7 +1,7 @@
 <header class="site-header">
 	<h1><a href="index.php">Web talk dog walk</a></h1>
 	<h2><?php perch_content('Location'); ?></h2>
+	<nav class="site-navigation">
+		<?php perch_pages_navigation(); ?>
+	</nav>
 </header>
-<nav class="site-navigation">
-	<?php perch_pages_navigation(); ?>
-</nav>
