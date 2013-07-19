88a7530fcb9cdbb2579acbdab7a1c469f43c0b87
laurakalbag <github@laurakalbag.com>
2013-07-19 14:53:50 +0100

    Add site-title class to site title

--- perch/templates/layouts/global/header.php
+++ perch/templates/layouts/global/header.php
@@ -1,5 +1,5 @@
 <header class="site-header">
-	<h1><a href="index.php">Web talk dog walk</a></h1>
+	<h1 class="site-title"><a href="index.php">Web talk dog walk</a></h1>
 	<h2><?php perch_content('Location'); ?></h2>
 	<nav class="site-navigation">
 		<?php perch_pages_navigation(); ?>
