a4e2f45885d9c7bd7b0a88d30f4ebafe4cbaa8b0
laurakalbag <github@laurakalbag.com>
2013-07-26 09:56:11 +0100

    Hard-code H1 logo link because I'm not sure how to work Perch with this crazy subfolder setup :-/

--- perch/templates/layouts/global/header.php
+++ perch/templates/layouts/global/header.php
@@ -1,6 +1,6 @@
 <header class="site-header">
 	<div class="header-wrap please-dont-hate-me">
-		<h1 class="site-title"><a href="index.php">Web talk dog walk</a></h1>
+		<h1 class="site-title"><a href="http://webtalkdogwalk.in/brighton">Web talk dog walk</a></h1>
 		<h2><?php perch_content('Location'); ?></h2>
 		<nav class="site-navigation">
 			<?php perch_pages_navigation(); ?>
